f283f7464b3d09632449f58c1bc20d4c3ce0c163
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-08-14 14:09:42 +0200

    feat() initial mocks and api design

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-export * from './greeter';
+import * as yow from 'yow-yo';
+import * as chalk from 'chalk';
+
+(async () => {
+  const o = chalk.green;
+  const r = chalk.red;
+
+  function logo() {
+    return `
+${o(' __   _______        __     ____ _     ___ ')}
+${o('   / / _        / /    / ___| |   |_ _|')}
+${o('   V / | |   / / /____| |   | |    | | ')}
+${r('   | || |_| | V  V /_____| |___| |___ | | ')}
+${r('   |_| ___/  _/_/       ____|_____|___| ')}
+    `;
+  }
+
+  const packageJson = require('../package.json');
+  const bsConfig = {
+    'auto-update': true,
+    'check-updates': true,
+    'cli-bin': 'yow',
+    description: 'Yow cli generator',
+    'logo-ascii': logo(),
+    name: packageJson.name,
+    registry: 'npm',
+    timeout: 2000,
+    version: packageJson.version,
+    'welcome-message': 'Welcome to YOW-cli',
+    'node-version': '>=6.0.0',
+  };
+  yow.baseConfig(bsConfig);
+})();
